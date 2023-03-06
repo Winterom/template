3984b4c8bdac75c66a70848ad2f86483b0f1ffec
winterom <winterom@gmail.com>
2023-03-06 22:30:14 +0300

    add ui checkbox

--- ngx_front/src/app/modules/admin/user-list/UsersTableDefinition.ts
+++ ngx_front/src/app/modules/admin/user-list/UsersTableDefinition.ts
@@ -3,12 +3,12 @@ import {SORT_DIRECTION, TableDefinition, VALUE_TYPE} from "../../../uikit/table/
 export class UsersTableDefinition extends TableDefinition{
   constructor() {
     super();
-    super.headerDefinition.push(
+    super.headerDefinition=[
       {title: 'id', sort: SORT_DIRECTION.ASC,type:VALUE_TYPE.NUMBER,width:'5em'},
       {title: 'email', sort: SORT_DIRECTION.DESC,type:VALUE_TYPE.STRING,width:'100%'},
       {title:'Создан',sort:SORT_DIRECTION.ASC,type:VALUE_TYPE.DATE,width:'8em'},
       {title:'Изменен',sort:SORT_DIRECTION.ASC,type:VALUE_TYPE.DATE,width:'8em'}
-    )
+    ];
     super.isCheckColumn=true;
   }
 }
